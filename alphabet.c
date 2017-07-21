bd3e0f27aa5fc3b34bf3566afa585e51497078f6
KamilaBanu <30333760+KamilaBanu@users.noreply.github.com>
2017-07-21 16:30:33 +0530

    alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char c;
+printf("enter the char:");
+scanf("%c",&c);
+if((c>='a'&& c>='z')//(c>='A'&& c>='Z'))
+printf("%c is alphabet",c);
+else
+printf("%c is not alphabet",c);
+}
