87cad57a4204e0f5da0550c11b2f802f68daed61
KamilaBanu <30333760+KamilaBanu@users.noreply.github.com>
2017-07-22 09:36:44 +0530

    vehicle.c

---
+++ vehicle.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int i,j,k;long long int count1=0,count2=0,s;
+for(j=65;j<=90;j++)
+{
+for(i=1;i<=9999;i++)
+{
+    count1++;
+}
+}{
+for(j=65;j<=90;j++)
+{
+    for(i=65;i<=90;i++)
+    {
+ 
+    for(k=1;k<=9999;k++)
+    {
+        count2++;
+    }
+}}}
+s=count1+count2;
+printf("%lld",s);
+return 0;
+}
