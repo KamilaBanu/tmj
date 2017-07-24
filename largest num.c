b2722d68b50986b94c147adafb24f66e129e147f
KamilaBanu <30333760+KamilaBanu@users.noreply.github.com>
2017-07-24 14:21:38 +0530

    largest num.c

---
+++ largest num.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("Enter three no:");
+scanf("%d %d %d",&a,&b,&c);
+if(a>= b &&a>= c)
+printf(" a is large");
+if(b>= a && b>= c)
+printf(" b is large");
+else
+if(c<=a&&c<=b)
+printf(" c is large");
+return 0;
+}
