b00b5d0710c1e9b374f18b7b03fd826909db3f35
KamilaBanu <30333760+KamilaBanu@users.noreply.github.com>
2017-07-24 11:19:26 +0530

    digits.c

--- digits.c
+++ digits.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 void main()
 {
-    int n,c=0;
-    printf("enter the number %d",n);
+    int a,c=0;
+    printf("enter the number %d",a);
     scanf("%d",&n);
     while(n>0)
     {
-        n=n/10;
+        a=a/10;
         c++;
     }
     printf("number of digits %d",c);
