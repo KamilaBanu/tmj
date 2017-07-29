2cafb5ae400ba6b4310e25776f2b6095ba1a66a8
KamilaBanu <30333760+KamilaBanu@users.noreply.github.com>
2017-07-29 16:44:46 +0530

    naturaln.c

---
+++ naturaln.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, s= 0;
+    
+    printf("%d",n);
+    scanf("%d",&n);
+
+    for(i=1; i <= n; ++i)
+    {
+        s+= i; 
+    }
+
+    printf("%d",s);
+
+    return 0;
+}
