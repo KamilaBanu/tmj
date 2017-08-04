63bf1e79ccb3a26fef4c7ebebb63483278791891
KamilaBanu <30333760+KamilaBanu@users.noreply.github.com>
2017-08-04 16:31:38 +0530

    loop.c

---
+++ loop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, s= 0;
+    
+    printf("%d",n);
+    scanf("%d",&n);
+
+    for(i=1; i <= n; i++)
+    {
+        s= i; 
+        s++;
+    }
+
+    printf("%d",s);
+
+    return 0;
+}
